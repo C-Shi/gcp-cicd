b55ec3741c830efbe236b380cf1233cd5a61d7f7
Cheng Shi <mr.cheng.shi@gmail.com>
2024-12-02 17:57:22 +0000

    v1.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to Python Flask World docker cloud run v2"
+    return "Welcome to Python Flask World docker cloud run v1.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
